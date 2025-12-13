bcb3e9e30553ad6d6e54e6546ecd22f2a626e21c
Dmitry Yaraev <damirworks@gmail.com>
2025-12-13 23:00:09 +0100

    Removed test-version4 with mike 2.1.3

--- test-version4/js/mike-versions-override.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var bust = "?v=" + new Date().getTime();
-    if (window.mikeVersionsUrl) {
-        window.mikeVersionsUrl = window.mikeVersionsUrl.split('?')[0] + bust;
-    } else {
-        // Fallback if the variable hasn't been set yet
-        window.mikeVersionsUrl = "/versions.json" + bust;
-    }
-})();
