0ceee64605886881f1781b42fc2b1f5530d4240c
Dmitry Yaraev <damirworks@gmail.com>
2026-02-10 00:12:07 +0100

    fix: add missing invocations of Thread.sleep for JMS sinks (#78)

--- connector-v1/src/main/scala/io/github/dyaraev/spark/connector/jms/v1/JmsSink.scala
+++ connector-v1/src/main/scala/io/github/dyaraev/spark/connector/jms/v1/JmsSink.scala
@@ -2,8 +2,8 @@ package io.github.dyaraev.spark.connector.jms.v1
 
 import io.github.dyaraev.spark.connector.jms.common.SourceSchema
 import io.github.dyaraev.spark.connector.jms.common.client.JmsSinkClient
+import io.github.dyaraev.spark.connector.jms.common.config.JmsSinkConfig
 import io.github.dyaraev.spark.connector.jms.common.config.MessageFormat.{BinaryFormat, TextFormat}
-import io.github.dyaraev.spark.connector.jms.common.config.{JmsConnectionConfig, JmsSinkConfig}
 import io.github.dyaraev.spark.connector.jms.common.utils.CommonUtils
 import org.apache.spark.internal.Logging
 import org.apache.spark.rdd.RDD
@@ -41,13 +41,13 @@ class JmsSink(config: JmsSinkConfig) extends Sink with Serializable with Logging
 
   private def sendBytesMessages(rdd: RDD[InternalRow], schema: Seq[Attribute], batchId: Long): Unit = {
     rdd.foreachPartition { iter =>
-      new JmsSink.BytesMessageSender(config.connection, schema).sendAll(iter, batchId)
+      new JmsSink.BytesMessageSender(config, schema).sendAll(iter, batchId)
     }
   }
 
   private def sendTextMessages(rdd: RDD[InternalRow], schema: Seq[Attribute], batchId: Long): Unit = {
     rdd.foreachPartition { iter =>
-      new JmsSink.TextMessageSender(config.connection, schema).sendAll(iter, batchId)
+      new JmsSink.TextMessageSender(config, schema).sendAll(iter, batchId)
     }
   }
 }
@@ -56,7 +56,7 @@ object JmsSink {
 
   private trait MessageSender[T] extends Logging {
 
-    protected def config: JmsConnectionConfig
+    protected def config: JmsSinkConfig
 
     protected def schema: Seq[Attribute]
 
@@ -78,6 +78,7 @@ object JmsSink {
             val message = fromRow(unsafeRow)
             send(client, message)
             counter += 1
+            safeSleep(client)
           }
         }
         commitJmsTransaction(client, counter)
@@ -95,7 +96,7 @@ object JmsSink {
     private def withClient(batchId: Long)(f: JmsSinkClient => Unit): Unit = {
       var client: JmsSinkClient = null
       try {
-        client = JmsSinkClient(config, transacted = true)
+        client = JmsSinkClient(config.connection, transacted = true)
         f(client)
       } catch {
         case NonFatal(e) =>
@@ -111,8 +112,7 @@ object JmsSink {
         logInfo(s"Committing $numMessages JMS messages")
         try client.commit()
         catch {
-          case NonFatal(e) =>
-            throw new RuntimeException("JMS commit failed", e)
+          case NonFatal(e) => throw new RuntimeException("JMS commit failed", e)
         }
       }
     }
@@ -120,13 +120,22 @@ object JmsSink {
     private def rollbackJmsTransaction(client: JmsSinkClient): Unit = {
       try client.rollback()
       catch {
-        case e: Throwable =>
-          logError("Unable to rollback transaction", e)
+        case e: Throwable => logError("Unable to rollback transaction", e)
+      }
+    }
+
+    private def safeSleep(client: JmsSinkClient): Unit = {
+      try config.throttlingDelayMs.foreach(Thread.sleep)
+      catch {
+        case e: InterruptedException =>
+          rollbackJmsTransaction(client)
+          Thread.currentThread().interrupt()
+          throw e
       }
     }
   }
 
-  private class TextMessageSender(override val config: JmsConnectionConfig, override val schema: Seq[Attribute])
+  private class TextMessageSender(override val config: JmsSinkConfig, override val schema: Seq[Attribute])
       extends MessageSender[String] {
 
     override protected def expressions: Seq[Expression] =
@@ -139,7 +148,7 @@ object JmsSink {
       client.sendTextMessage(message)
   }
 
-  private class BytesMessageSender(override val config: JmsConnectionConfig, override val schema: Seq[Attribute])
+  private class BytesMessageSender(override val config: JmsSinkConfig, override val schema: Seq[Attribute])
       extends MessageSender[Array[Byte]] {
 
     override protected def expressions: Seq[Expression] =
